5089b1e486f52cd1c5d933bb499b8b9ea8b58fd4
e-bravodey <deysi.bravo.extern@diconium.com>
2024-02-20 19:43:11 +0100

    Assignment adding test beer patch not found

--- src/test/java/guru/springframework/spring6reactive/controllers/BeerControllerTest.java
+++ src/test/java/guru/springframework/spring6reactive/controllers/BeerControllerTest.java
@@ -45,6 +45,15 @@ class BeerControllerTest {
     }
 
     @Test
+    void testPatchBeerNotFound() {
+        webTestClient.patch()
+                .uri(BeerController.BEER_PATH_ID, 999)
+                .body(Mono.just(BeerRepositoryTest.getTestBeer()), BeerDTO.class)
+                .exchange()
+                .expectStatus().isNotFound();
+    }
+
+    @Test
     void testUpdateBeerNotFound() {
         webTestClient.put()
                 .uri(BeerController.BEER_PATH_ID, 999)
